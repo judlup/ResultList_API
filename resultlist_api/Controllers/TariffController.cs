5c7e365cbda754e410477482da4c2f17a825a33e
Judlup <judlup@gmail.com>
2021-12-14 00:26:49 -0500

    first commit

---
+++ resultlist_api/Controllers/TariffController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using resultlist_api.Models;
+
+namespace resultlist_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TariffController : ControllerBase
+    {        
+        [HttpGet(Name = "GetTest")]
+        public dynamic Get()
+        {
+            dynamic jsonFromFile;
+            using(var reader =  new StreamReader("./Data/Data.json"))
+            {
+                jsonFromFile = reader.ReadToEnd();
+            }
+            
+            return Ok(jsonFromFile);
+        }
+    } 
+}
---
+++ resultlist_api/Models/Tariff.cs
@@ -0,0 +1,11 @@
+﻿namespace resultlist_api.Models
+{
+    public class Tariff
+    {
+        public string Name { get; set; }
+        public int Download { get; set; }
+        public int Upload { get; set; }
+        public string[] Benefits { get; set; }
+        public double Price { set; get; }
+    }
+}
